819ec46ea702795efbd25e2b1f5d16088eca7aff
oldvoskres29293828 <turpalelihanov76@gmail.com>
2025-07-02 15:26:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 import os
 
-BOT_TOKEN = os.getenv("7797420435:AAE7y4ejBM14mHyQ4ZtJ7mDkCL15hXw9now")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start(msg: types.Message):
-    await msg.answer("Привет! Я на Scalingo 🚀")
+    await msg.answer("Привет! Я работаю на Scalingo 🚀")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
