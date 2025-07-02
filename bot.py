aafd92ab25408d88f66fff4ad84154d55535d593
oldvoskres29293828 <turpalelihanov76@gmail.com>
2025-07-02 15:12:10 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(msg: types.Message):
+    await msg.answer("Привет! Я на Scalingo 🚀")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
